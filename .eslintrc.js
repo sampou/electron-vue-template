a43147009a034c60500cbcdbac6f2eff420b0959
sampou@163.com <sampou@163.com>
2022-05-17 22:15:02 +0800

    优化

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    root: true,
+    parser: 'vue-eslint-parser',
+    extends: ['airbnb-base', 'plugin:vue/essential', 'standard', 'plugin:prettier/recommended'],
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+    },
+    parserOptions: {
+        node: true,
+        sourceType: 'module',
+        ecmaVersion: 2021,
+        parser: '@typescript-eslint/parser',
+    },
+    plugins: ['@typescript-eslint', 'vue', 'html'],
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+            },
+        },
+        'import/extensions': ['.js', '.ts', '.mjs', '.jsx', '.tsx', '.vue'],
+    },
+    globals: {
+        APP_ENV: 'readonly',
+        MODE: 'readonly',
+        VERSION: 'readonly',
+        RENDER_ENV: 'readonly',
+        NodeJS: 'readonly',
+        Electron: 'readonly',
+    },
+    rules: {
+        'spaced-comment': ['error', 'always'],
+        'no-tabs': 'off',
+        'vue/comment-directive': 'off',
+        'vue/no-multiple-template-root': 'off',
+        'vue/multi-word-component-names': 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    },
+}
